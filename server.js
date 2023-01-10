9820c6353bfe8c144dfa9e0892b059d50a174f40
Sergio Ortiz <owes423349@gmail.com>
2023-01-10 09:56:58 -0500

    build 2

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 // @see https://strapi.io/documentation/v3.x/getting-started/deployment.html#application-configuration
 // server.js content:
 
-// const strapi = require("strapi");
-//Build
-// strapi(/* {...} */).start();
-const strapi = require("strapi");
+const strapi = require("@strapi/strapi");
+strapi(/* {...} */).start();
 
-if (process.env.NODE_ENV == "development")
-  strapi({ autoReload: { enabled: true } }).start();
-else strapi().start();
+// const strapi = require("strapi");
+
+// if (process.env.NODE_ENV == "development")
+//   strapi({ autoReload: { enabled: true } }).start();
+// else strapi().start();
