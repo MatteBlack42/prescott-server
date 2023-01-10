99bd7760e3c7cf4052bc232cf09a618ba6fa5b42
Sergio Ortiz <owes423349@gmail.com>
2023-01-10 09:09:53 -0500

    server

---
+++ server.js
@@ -0,0 +1,7 @@
+// server.js
+// @see https://strapi.io/documentation/v3.x/getting-started/deployment.html#application-configuration
+// server.js content:
+
+const strapi = require("strapi");
+
+strapi(/* {...} */).start();
